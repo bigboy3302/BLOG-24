7e385fad202738d742e8f1b5fc564af3c1046ac1
Skolnieks <skolnieks.pjvt@pjvt.local>
2024-02-26 09:03:29 +0200

    lil gypsy

---
+++ index.php
@@ -0,0 +1,13 @@
+-- CREATE DATABASE`BLOG ipb-22`;
+/*
+CREATE TABLE posts (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+title VARCHAR(255) NOT NULL
+);
+*/
+
+INSERT INTO posts
+(TITLE)
+VALUE
+("My First Blog Post"),
+("My Second Blog Post");
