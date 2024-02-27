d57c03564fa703e67306deb6ac76a9183b3ebc5b
Skolnieks <skolnieks.pjvt@pjvt.local>
2024-02-27 13:33:50 +0200

    penis

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php 
+
+require "functions.php";
+
 echo "hello";
 
 $connection_string = "mysql:host=localhost;
@@ -13,10 +16,13 @@ $query = $pdo-> prepare("SELECT * FROM posts");
 $query-> execute();
 $posts = $query-> fetchAll(PDO::FETCH_ASSOC);
 
-echo "<pre>";
-var_dump($posts);
-echo "</pre>";
 
+
+echo"<ul>";
+foreach($posts as $post){
+    echo"<li>" . $post ["title"] . "</li>";
+}
+echo"</ul>";
 
 
 ?>
