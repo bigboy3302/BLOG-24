e956a6317b6e3765731de48a2be4dc097b226c7e
Skolnieks <skolnieks.pjvt@pjvt.local>
2024-03-05 12:38:58 +0200

    yeh

---
+++ Config.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    "host" => "localhost",
+    "dbname" => "blog_ipb-22",
+    "user" => "root",
+    "password" => "",
+    "charset" => "utf8mb4"
+];
+
--- index.php
+++ index.php
@@ -1,26 +1,41 @@
-<?php 
+<?php
 
 require "functions.php";
 require "Database.php";
-$config = [
-    "host" => "localhost",
-    "dbname" => "blog_ipb-22",
-    "user" => "root",
-    "password" => "",
-    "charset" => "utf8mb4"
-];
+$config = require("Config.php");
 
-$db = new Database();
-$posts = $db
-            ->execute("SELECT * FROM posts")
-            -> fetchAll();
+// Initialize query to fetch all posts by default
+$query = "SELECT * FROM posts";
 
-
-echo"<ul>";
-foreach($posts as $post){
-    echo"<li>" . $post ["title"] . "</li>";
+// Check if id is set in $_GET and not empty, then update the query accordingly
+if (isset($_GET["id"]) && $_GET["id"] !== "") {
+    $id = $_GET["id"];
+    // Sanitize the input to prevent SQL injection
+    $id = intval($id);
+    // Update the query to fetch a specific post with the given id
+    $query = "SELECT * FROM posts WHERE id=$id";
 }
-echo"</ul>";
 
+// Initialize Database class
+$db = new Database();
+
+// Execute the query and fetch posts
+$posts = $db
+    ->execute($query)
+    ->fetchAll();
+
+// Start HTML form
+echo "<form method='GET'>"; 
+echo "<input type='text' name='id'/>"; // Changed 'name' attribute to 'id' and added 'type' attribute
+echo "<button type='submit'>Submit</button>"; 
+echo "</form>";
+
+// Display posts
+echo "<h1>POSTS</h1>";
+echo "<ul>";
+foreach ($posts as $post) {
+    echo "<li>" . $post["title"] . "</li>";
+}
+echo "</ul>";
 
 ?>
